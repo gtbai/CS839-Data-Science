7848138d3b0acdcaea097c371f2087d2b1b1718f
NostalgiaAlex <sunchangtian@126.com>
2019-04-08 01:06:25 -0500

    tmdb before multiprocessing, before earthquake

--- stage2/src/crawler_tmdb.py
+++ stage2/src/crawler_tmdb.py
@@ -1,12 +1,34 @@
 import requests
+import re
 from bs4 import BeautifulSoup as BS
 
 
 TMDB_MOVIE_LIST_URL = 'https://www.themoviedb.org/movie?page='
 TMDB_BASE_URL = 'https://www.themoviedb.org'
 
-total_number = 20 # TODO: change this to 4000
-movies_per_page = 20
+TOTAL_NUMBER = 20 # TODO: change this to 4000
+MOVIES_PER_PAGE = 20
+
+
+def get_crew_list(soup, type):
+    list = []
+    crew = soup.find('h4', text=type).parent
+    div_parts = crew.find_all('div', class_='info')
+    for div in div_parts:
+        list.append(div.find('a').get_text())
+    return ';'.join(list)
+
+
+def get_actors(soup):
+    list = []
+    candidates = soup.find_all('div', class_='split')
+    for candidate in candidates:
+        if candidate.find('h3') and candidate.find('h3').get_text().startswith('Cast'):
+            lis = candidate.find_all('li')
+            for li in lis:
+                list.append(li.find('div', class_='info').find('a').get_text())
+            break
+    return ';'.join(list)
 
 
 def get_movie_info(home_url):
@@ -16,24 +38,53 @@ def get_movie_info(home_url):
     info = []
     home_page = requests.get(home_url)
     home_soup = BS(home_page.content, 'html.parser')
-    crew_url_suffix = home_soup.find('a', text="Full Cast & Crew").get('href')
-    crew_page = requests.get(TMDB_BASE_URL + crew_url_suffix)
-    crew_soup = BS(crew_page.content, 'html.parser')
-    # name
-    name = home_soup.find('h2').get_text()
-    info.append(name)
-    print("Name: " + name)
+    cast_crew_url_suffix = home_soup.find('a', text='Full Cast & Crew').get('href')
+    cast_crew_page = requests.get(TMDB_BASE_URL + cast_crew_url_suffix)
+    cast_crew_soup = BS(cast_crew_page.content, 'html.parser')
+    # title
+    title = home_soup.find('h2').get_text()
+    info.append(title)
+    # print("Name: " + title)
     # year
-    year = home_soup.find('span', class_="release_date").get_text()
+    year = home_soup.find('span', class_='release_date').get_text()
     year = year[1:-1]
-    # directors
-
-    # writers
-    # actors
+    info.append(year)
+    # genres
+    genres = []
+    genre_sec = home_soup.find('bdi', text='Genres').parent.parent.find('ul').find_all('li')
+    for g in genre_sec:
+        genres.append(g.find('a').get_text())
+    info.append(';'.join(genres))
+    # language
+    language_tag = home_soup.find('bdi', text='Original Language')
+    raw_language = language_tag.parent.parent.get_text()
+    language = raw_language.split(' ')[2]
+    info.append(language)
     # runtime
+    runtime_tag = home_soup.find('bdi', text='Runtime')
+    raw_time = runtime_tag.parent.parent.get_text()
+    split_time = raw_time.split()
+    runtime = int(split_time[1][:-1]) * 60 + int(split_time[2][:-1])
+    info.append(str(runtime))
     # budget
+    budget_tag = home_soup.find('bdi', text='Budget')
+    raw_budget = budget_tag.parent.parent.get_text().split()[1]
+    budget = raw_budget[1:].split('.')[0].replace(',','')
+    info.append(budget)
     # revenue
-    # genre
+    revenue_tag = home_soup.find('bdi', text='Revenue')
+    raw_revenue = revenue_tag.parent.parent.get_text().split()[1]
+    revenue = raw_revenue[1:].split('.')[0].replace(',','')
+    info.append(revenue)
+    # directors
+    directors = get_crew_list(cast_crew_soup, "Directing")
+    info.append(directors)
+    # writers
+    writers = get_crew_list(cast_crew_soup, "Writing")
+    info.append(writers)
+    # actors
+    actors = get_actors(cast_crew_soup)
+    info.append(actors)
     return info
 
 
@@ -53,5 +104,6 @@ def get_movies_in_page(movie_list_url):
 
 if __name__ == '__main__':
     movies = []
-    for page_no in range(int(total_number / movies_per_page)):
+    for page_no in range(int(TOTAL_NUMBER / MOVIES_PER_PAGE)):
         movies.extend(get_movies_in_page(TMDB_MOVIE_LIST_URL + str(page_no + 1)))
+    print(movies)
